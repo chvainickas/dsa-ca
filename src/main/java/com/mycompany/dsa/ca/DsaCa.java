2aece35419b59e19a76f4bbe5a420885850321df
chvainickas <chvainickas@gmail.com>
2024-03-12 09:23:19 +0000

    init project

---
+++ src/main/java/com/mycompany/dsa/ca/DsaCa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dsa.ca;
+
+/**
+ *
+ * @author edward
+ */
+public class DsaCa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
